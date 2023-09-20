205ff477d5e3e16d6961693d5ccc1c8406c70b21
JohnLYYN123 <96747053+JohnLYYN123@users.noreply.github.com>
2023-09-20 00:57:00 -0400

    Activity 3 is completed
    The homepage has a navigation bar, hello name! message, and current time

--- hello.py
+++ hello.py
@@ -1,19 +1,37 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-# hello world homepage
+# homepage
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", current_time=datetime.utcnow())
+
 
 # example 2.2 part
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", name=name)
+
+
+# 404 error handler
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# 500 error handler
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 
 
